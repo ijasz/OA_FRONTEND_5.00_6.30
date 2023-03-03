97faef42aa738a203af0277e3656e3b3fe8e6b04
“ijasz” <jass07rtr@gmail.com>
2023-03-03 18:50:53 +0530

    updated on 03-03-2023

---
+++ JS/primitive_reference_type/index.js
@@ -0,0 +1,39 @@
+// primitive type
+
+// There are 8 primitive data types:
+//string.
+//number.
+//bigint.
+//boolean.
+//undefined.
+//symbol.
+//null.
+//NaN
+
+// var a = 1232n; //memory 545fd
+// var b = a; //memory 545ertcx
+// var c = a; //memory 545rd4fd
+
+// b = 20;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// Reference Type
+
+var a = [1, 2, 3, 4];
+var b = a;
+var c = a;
+var d = a;
+
+d = [55, 34];
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
+
+
