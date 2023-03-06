6f7d81110fa225c8f7d9eb2e61af6371ed9fce64
“ijasz” <jass07rtr@gmail.com>
2023-03-06 18:48:38 +0530

    scope & debug

---
+++ JS/scopes&debug/index.js
@@ -0,0 +1,44 @@
+console.log("scope");
+//var memory execution goes global scope
+
+// console.log(a);
+
+// var a = 10;
+
+// console.log(a);
+
+// let&const memory execution goes script scope
+
+// const b = 10;
+
+// console.log(b);
+
+// block scope
+
+// {
+//     var c = 20;
+//     let d = 30;
+//     console.log(d);
+// }
+
+// console.log(c);
+
+// reversed words
+
+// let for = 524;
+
+// const
+
+const arr = ["a", "b", "c"];
+
+arr[0] = "changed"
+
+console.log(arr);
+
+const obj = {
+    name: "xxx"
+}
+
+obj.subject = "yyy"
+
+console.log(obj);
