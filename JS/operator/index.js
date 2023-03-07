875b29143a268fac5e43ff6c37712d84abcab310
“ijasz” <jass07rtr@gmail.com>
2023-03-07 19:01:38 +0530

    operator

---
+++ JS/operator/index.js
@@ -0,0 +1,88 @@
+// Arithmetic Operators
+
+// console.log(1 + 2);
+// console.log(1 - 2);
+// console.log(1 * 2);
+// console.log(1 % 2);
+// console.log(2 ** 2);
+// console.log(Math.pow(2, 2));
+// console.log(1 / 2);
+
+// post Increment
+
+// let a = 1;
+
+// let post = a++;
+
+// console.log(a, post);
+
+// pre Increment
+
+// let b = 1;
+
+// let pre = ++b;
+
+// console.log(b, pre);
+
+
+// let task = 1;
+
+// console.log(++task);
+// console.log(task--);
+// console.log(task--);
+// console.log(task++);
+// console.log(--task);
+
+// Assignment Operators
+
+// let a = 1;
+
+// a = a + 1;
+
+// a += 1
+// a -= 1
+// a *= 2
+
+// console.log(a);
+
+// Comparison Operators
+
+// == checks only values
+
+// console.log("1" == 1);
+// console.log("1" === 1);
+
+// console.log(1 != 1);
+// console.log("1" != 1);
+// console.log("1" !== 1);
+// console.log(1 !== 1);
+
+// console.log(1 > 3);
+// console.log(1 < 3);
+// console.log(3 >= 3);
+// console.log(3 <= 3);
+
+//ternary operator
+
+// const age = Number(prompt("enter your age"))
+
+// console.log(age >= 18 ? "eligible for vote" : "not eligible for vote");
+
+// Logical Operators
+
+// console.log(!(2 > 2) && !(4 > 6));
+// console.log(!(2 > 2) || 4 > 6);
+
+// concatenate
+
+// let text1 = "sharan";
+// let text2 = "eswar";
+// let text3 = text1 + " " + text2;
+
+// console.log(text3);
+
+// window.print()
+
+
+
+
