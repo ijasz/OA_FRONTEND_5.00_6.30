98d8e44600b6b0f36b2f6f160eee21106e302159
“ijasz” <jass07rtr@gmail.com>
2023-03-08 18:40:53 +0530

    added files

---
+++ JS/conditional_statement/index.js
@@ -0,0 +1,21 @@
+if (false) {
+    console.log("if");
+} else if (false) {
+    console.log("else if");
+} else {
+    console.log("else");
+}
+
+if (false) {
+    console.log("if1");
+}
+if (true) {
+    console.log("if2");
+}
+if (false) {
+    console.log("if3");
+}
+
+else {
+    console.log("else");
+}
---
+++ JS/exceution_context/index.js
@@ -0,0 +1,19 @@
+// a = 10;
+// b = 20;
+// c = 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var a;
+// var b;
+// var c;
+
+// console.log(a);
+
+// let a;
+
+// const b = 10;
+
+// console.log(b);
