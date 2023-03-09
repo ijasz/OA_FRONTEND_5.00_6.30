3b4f39df86b01f1f4537ddb197d1cecc6486d1b0
“ijasz” <jass07rtr@gmail.com>
2023-03-09 18:29:18 +0530

    switch case & loops

--- JS/conditional_statement/index.js
+++ JS/conditional_statement/index.js
@@ -1,21 +1,77 @@
-if (false) {
-    console.log("if");
-} else if (false) {
-    console.log("else if");
-} else {
-    console.log("else");
-}
+// if (false) {
+//     console.log("if");
+// } else if (false) {
+//     console.log("else if");
+// } else {
+//     console.log("else");
+// }
 
-if (false) {
-    console.log("if1");
-}
-if (true) {
-    console.log("if2");
-}
-if (false) {
-    console.log("if3");
-}
+// if (false) {
+//     console.log("if1");
+// }
+// if (true) {
+//     console.log("if2");
+// }
+// if (false) {
+//     console.log("if3");
+// }
 
-else {
-    console.log("else");
+// else {
+//     console.log("else");
+// }
+
+
+// if (false) {
+//     console.log("if");
+//     if (true) {
+//         console.log("nested if");
+//     }
+// } else {
+//     console.log("else");
+// }
+
+
+const date = new Date()
+
+console.log(typeof date.getDay());
+console.log(date.getDay());
+
+const h1 = window.document.body.getElementsByTagName("h1")[0]
+
+console.log(h1);
+
+
+switch (date.getDay()) {
+    case 0:
+        h1.textContent = "sun"
+        // console.log("sun");
+        break;
+    case 1:
+        h1.textContent = "mon"
+        // console.log("mon");
+        break;
+    case 2:
+        h1.textContent = "tue"
+        // console.log("tue");
+        break;
+    case 3:
+        h1.textContent = "wed"
+        // console.log("wed");
+        break;
+    case 4:
+        h1.textContent = "thr"
+        // console.log("thr");
+        break;
+    case 5:
+        h1.textContent = "fri"
+        // console.log("fri");
+        break;
+    case 6:
+        h1.textContent = "sat"
+        // console.log("sat");
+        break;
+
+    default:
+        console.log("invalid");
+        break;
 }
---
+++ JS/loops/index.js
@@ -0,0 +1,74 @@
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+
+// let i;
+
+// for (i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// console.log("");
+
+// for (i = 5; i >= 0; i--) {
+//     console.log(i);
+// }
+
+// console.log("");
+
+// for (let i = 0; i <= 2; i++) {
+//     for (let j = 0; j <= 2; j++) {
+//         console.log(i, j);
+//     }
+// }
+
+// array iteration -> for of
+
+// const arr = ["kamal", "mukesh", "mani", "sam"]
+
+// for (const i of arr) {
+//     console.log(i);
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(i);
+// }
+
+// not to prefer for in to array
+// for (const i in arr) {
+//     console.log(i);
+// }
+
+
+// object iteration -> for in
+
+
+// const obj = {
+//     name: "xxx",
+//     age: 10,
+//     designation: "developer"
+// }
+
+// for (const key in obj) {
+//     console.log(`${key} : ${obj[key]}`);
+// }
+
+// while loop
+
+// let i = 0;
+
+// while (i < 10) {
+//     console.log(++i);
+// }
+
+// console.log("");
+
+// j = 0;
+
+// do {
+//     console.log(++j);
+// } while (j < 10);
+
+
